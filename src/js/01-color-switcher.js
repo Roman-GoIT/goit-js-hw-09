c022fa365cc1c89a2d2731d793a9ae33969394d2
Roman-GoIT <best4you.ua@gmail.com>
2023-05-02 18:41:40 +0300

    First HW

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,26 @@
+const start = document.querySelector('button[data-start]');
+const stop = document.querySelector('button[data-stop]');
+const background = document.querySelector('body');
+let intId = null;
 
+start.addEventListener('click', onStart);
+
+function onStart() {
+    start.disabled = true;
+  intId = setInterval(() => {
+    background.style.backgroundColor = getRandomHexColor();
+  }, 1000);
+}
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+}
+
+stop.addEventListener('click', onStop)
+
+function onStop() {
+    start.disabled = false;
+    clearInterval(intId)
+}
